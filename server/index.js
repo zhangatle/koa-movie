c57b1df90001679ac9e60fb0eef8f1d02f33fd9e
zhangle <319126583@qq.com>
2018-12-26 11:57:20 +0800

    完善电影的爬取

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const Koa = require('koa');
 const views = require("koa-views");
 const {resolve} = require("path");
-const {connect} = require('./database/init');
+const {connect, initSchemas} = require('./database/init');
+const router = require('./routes');
 const app = new Koa();
 
 ;(async () => {
     await connect();
+    initSchemas();
+    // require('./tasks/api');
+
 })();
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.use(views(resolve(__dirname, './views'), {
     extension: 'pug'
--- server/tasks/api.js
+++ server/tasks/api.js
@@ -1,4 +1,7 @@
 const rp = require('request-promise-native');
+const mongoose = require('mongoose');
+const Movie = mongoose.model('Movie');
+const Category = mongoose.model('Category');
 
 async function fetchMovie(item) {
     const url = `http://api.douban.com/v2/movie/${item.doubanId}`;
@@ -15,22 +18,78 @@ async function fetchMovie(item) {
 }
 
 ;(async () => {
-   let movies = [
-       { doubanId: 30358789,
-           title: '潮间奇事',
-           rate: 6.2,
-           poster: 'https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2540641704.jpg'
-       },
-       { doubanId: 27092648,
-           title: '蒙上你的眼',
-           rate: 6.9,
-           poster: 'https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2537908150.jpg'
-       },
-   ];
+   let movies = await Movie.find({
+       $or: [
+           {summary: {$exists: false}},
+           {summary: null},
+           {year: {$exists: false}},
+           {title: ''},
+           {summary: ''}
+       ]
+   });
 
    for(let i = 0; i < movies.length; i++){
        let movie = movies[i];
        let movieData = await fetchMovie(movie);
        console.log(movieData);
+       if(movieData){
+           let tags = movieData.tags || [];
+           movie.tags = movie.tags || [];
+           movie.summary = movieData.summary || '';
+           movie.title = movieData.alt_title || movieData.title || '';
+           movie.tawTitle = movieData.title || '';
+
+           if(movieData.attrs){
+                movie.movieTypes = movieData.attrs.movie_type || [];
+                movie.year = movieData.attrs.year[0] || 2500;
+                for(let i = 0; i < movie.movieTypes.length; i++){
+                    let item = movie.movieTypes[i];
+                    let cat = await Category.findOne({
+                        name: item
+                    });
+                    if(!cat) {
+                       cat = new Category({
+                            name: item,
+                            movies: [movie._id]
+                        })
+                    }else {
+                        if(cat.movies.indexOf(movie._id) === -1){
+                            cat.movies.push(movie._id);
+                        }
+                    }
+
+                    await cat.save();
+
+                    if(!movie.category){
+                        movie.category.push(cat._id);
+                    }else {
+                        if(movie.category.indexOf(cat._id) === -1){
+                            movie.category.push(cat._id);
+                        }
+                    }
+                }
+                let dates = movieData.attrs.pubdate || [];
+                let pubdates = [];
+                dates.map(item => {
+                    if(item && item.split('(').length > 0) {
+                        let parts = item.split('(');
+                        let date = parts[0];
+                        let country = '未知';
+                        if(parts[1]){
+                            country = parts[1].split(')')[0];
+                        }
+                        pubdates.push({
+                            date: new Date(date),
+                            country
+                        })
+                    }
+                });
+                movie.pubdate = pubdates;
+           }
+           tags.forEach(tag => {
+               movie.tags.push(tag.name);
+           });
+           await movie.save();
+       }
    }
 })();
