6b72433bd95598e7341b2e1005e0365a6e561d32
zhangle <319126583@qq.com>
2018-12-29 18:38:59 +0800

    更新启动文件

--- start.js
+++ start.js
@@ -1,3 +1,3 @@
-require('babel-core/register')();
-require('babel-polyfill');
-require('./server/index.js');
+require('babel-core/register')() //运行一个es6并不支持的，需要运行时
+require('babel-polyfill')
+require('./server/index.js')
